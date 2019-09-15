5db433f58275b5d3efae2ce80516ef9a06f7b9a5
Shujathlive <shujathlive@gmail.com>
2019-09-15 21:44:04 +0530

    changes

--- src/app/cursor/cursor.component.ts
+++ src/app/cursor/cursor.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import TweenMax from '../../assets/js/TweenMax.min.js';
+import * as paper from 'paper';
+import {Color} from 'paper';
+// import * as paper from '../../assets/js/paper-core.min.js';  // Run `npm install --save
+import * as SimplexNoise from '../../assets/js/simplex-noise.min.js';  // Run `npm install --save
 @Component({
   selector: 'app-cursor',
   templateUrl: './cursor.component.html',
@@ -15,39 +19,95 @@ export class CursorComponent implements OnInit {
     const innerCursor = (document.querySelector('.cursor--small') as HTMLDivElement);
     // this.loadScript('../assets/js/TweenMax.min.js');
     const initCursor = () => {
-    // add listener to track the current mouse position
-    document.addEventListener('mousemove', e => {
-      clientX = e.clientX;
-      clientY = e.clientY;
-    });
-
-    // transform the innerCursor to the current mouse position
-    // use requestAnimationFrame() for smooth performance
-    const render = () => {
-     // innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
-      // if you are already using TweenMax in your project, you might as well
-      // use TweenMax.set() instead
-      TweenMax.set(innerCursor, {
-        x: clientX,
-        y: clientY
+      // add listener to track the current mouse position
+      document.addEventListener('mousemove', e => {
+        clientX = e.clientX;
+        clientY = e.clientY;
       });
 
+      // transform the innerCursor to the current mouse position
+      // use requestAnimationFrame() for smooth performance
+      const render = () => {
+        // innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+        // if you are already using TweenMax in your project, you might as well
+        // use TweenMax.set() instead
+        TweenMax.set(innerCursor, {
+          x: clientX,
+          y: clientY
+        });
+
+        requestAnimationFrame(render);
+      };
       requestAnimationFrame(render);
-    };
-    requestAnimationFrame(render);
     };
 
     initCursor();
+    let lastX = 0;
+    let lastY = 0;
+    let isStuck = false;
+    let showCursor = false;
+    let group, stuckX, stuckY, fillOuterCursor;
+
+    const initCanvas = () => {
+      const canvas = (document.querySelector('.cursor--canvas') as HTMLCanvasElement);
+      const shapeBounds = {
+        width: 75,
+        height: 75
+      };
+      paper.setup(canvas);
+      const strokeColor = "'#000000'";
+      const strokeWidth = 1;
+      const segments = 8;
+      const radius = 10;
+
+      // we'll need these later for the noisy circle
+      const noiseScale = 150; // speed
+      const noiseRange = 4; // range of distortion
+      let isNoisy = true; // state
+
+      // the base shape for the noisy circle
+      const polygon = new paper.Path.RegularPolygon(
+        new paper.Point(1, 1),
+        segments,
+        radius
+      );
+      polygon.strokeColor = new Color(0, 0, 0);
+      polygon.strokeWidth = strokeWidth;
+      polygon.smooth();
+      group = new paper.Group([polygon]);
+      group.applyMatrix = false;
+
+      const noiseObjects = polygon.segments.map(() => new SimplexNoise());
+      let bigCoordinates = [];
+      
+      // function for linear interpolation of values
+      const lerp = (a, b, n) => {
+        return (1 - n) * a + n * b;
+      };
+
+      // function to map a value from one range to another range
+      const map = (value, in_min, in_max, out_min, out_max) => {
+        return (
+          ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+        );
+      };
+
+      // the draw loop of Paper.js 
+      // (60fps with requestAnimationFrame under the hood)
+      paper.view.onFrame = event => {
+        // using linear interpolation, the circle will move 0.2 (20%)
+        // of the distance between its current position and the mouse
+        // coordinates per Frame
+        lastX = lerp(lastX, clientX, 0.2);
+        lastY = lerp(lastY, clientY, 0.2);
+        group.position = new paper.Point(lastX, lastY);
+      }
+    }
+
+
+    initCanvas();
   }
 
-  public loadScript(url: string) {
-    const body = <HTMLDivElement> document.body;
-    const script = document.createElement('script');
-    script.innerHTML = '';
-    script.src = url;
-    script.async = false;
-    script.defer = true;
-    body.appendChild(script);
-  }
+
 
 }
--- src/app/engine/engine.service.ts
+++ src/app/engine/engine.service.ts
@@ -12,29 +12,29 @@ export class EngineService {
   private scene: THREE.Scene;
   private light: THREE.AmbientLight;
   private mouseX = 0;
-  private  mouseY = 0;
+  private mouseY = 0;
   private windowHalfX = window.innerWidth / 2;
-			private windowHalfY = window.innerHeight / 2;
-  createGeometry = function(){
+  private windowHalfY = window.innerHeight / 2;
+  createGeometry = function () {
     var geometry = new THREE.BufferGeometry();
-				var vertices = [];
-				var vertex = new THREE.Vector3();
-				for ( var i = 0; i < 500; i ++ ) {
-					vertex.x = Math.random() * 2 - 1;
-					vertex.y = Math.random() * 2 - 1;
-					vertex.z = Math.random() * 2 - 1;
-					vertex.normalize();
-					vertex.multiplyScalar( 350 );
-					vertices.push( vertex.x, vertex.y, vertex.z );
-					vertex.multiplyScalar( Math.random() * 0.2 + 1 );
-					vertices.push( vertex.x, vertex.y, vertex.z );
-				}
-				geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-				return geometry;
+    var vertices = [];
+    var vertex = new THREE.Vector3();
+    for (var i = 0; i < 150; i++) {
+      vertex.x = Math.random() * 4 - 1;
+      vertex.y = Math.random() * 2 - 1;
+      vertex.z = Math.random() * 4 - 1;
+      vertex.normalize();
+      vertex.multiplyScalar(550);
+      vertices.push(vertex.x, vertex.y, vertex.z);
+      vertex.multiplyScalar(Math.random() * 0.2 + 1);
+      vertices.push(vertex.x, vertex.y, vertex.z);
+    }
+    geometry.addAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    return geometry;
   }
   // onDocumentMouseMove(event){
   //   this.mouseX = event.clientX - this.windowHalfX;
-	// 			this.mouseY = event.clientY - this.windowHalfY;
+  // 			this.mouseY = event.clientY - this.windowHalfY;
   // }
 
   // onDocumentTouchStart(event) {
@@ -55,7 +55,7 @@ export class EngineService {
     // The first step is to get the reference of the canvas element from our HTML document
     this.canvas = <HTMLCanvasElement>document.getElementById(elementId);
 
-    
+
     // create the scene
     this.scene = new THREE.Scene();
 
@@ -66,35 +66,36 @@ export class EngineService {
     this.scene.add(this.camera);
 
     // soft white light
-    this.light = new THREE.AmbientLight( 0x4f4f4f );
+    this.light = new THREE.AmbientLight(0x4f4f4f);
     this.light.position.z = 10;
     this.scene.add(this.light);
-    let i, line,material, p, parameters = [ [ 0.25, 0x000000, 2 ], [ 0.5, 0x000000, 2 ], [ 0.75, 0x000000, 0.75 ], [ 1, 0x000000, 0.5 ], [ 1.25, 0x000000, 0.8 ],
-							       [ 3.0, 0x000000, 0.75 ], [ 3.5, 0x000000, 0.5 ], [ 4.5, 0x000000, 0.25 ], [ 5.5, 0x000000, 0.125 ] ];
+    let i, cube, material, p, parameters = [[0.25, 0x000000, 2], [0.5, 0x000000, 2], [0.75, 0x000000, 0.75], [1, 0x000000, 0.5], [1.25, 0x000000, 0.8],
+    [3.0, 0x000000, 0.75], [3.5, 0x000000, 0.5], [4.5, 0x000000, 0.25], [5.5, 0x000000, 0.125]];
 
     let geometry = this.createGeometry();
     // i=1
-    for( i = 0; i < parameters.length; ++ i ) {
-      p = parameters[ i ];
-      material = new THREE.LineBasicMaterial( { color: p[ 1 ], opacity: p[ 2 ] } );
-      line = new THREE.LineSegments( geometry, material );
-      line.scale.x = line.scale.y = line.scale.z = p[ 0 ];
-      line.userData.originalScale = p[ 0 ];
-      line.rotation.y = Math.random() * Math.PI * 4;
-      
-      line.rotation.z = Math.random() * Math.PI;
-      line.rotation.x = Math.random() * (Math.PI/6);
-      line.updateMatrix();
-      this.scene.add( line );
-      
+
+    for (i = 0; i < parameters.length; ++i) {
+      p = parameters[i];
+      material = new THREE.MeshBasicMaterial({ color: p[1], opacity: p[2] });
+      cube = new THREE.Mesh(geometry, material);
+      // line.scale.x = line.scale.y = line.scale.z = p[ 0 ];
+      // line.userData.originalScale = p[ 0 ];
+      cube.rotation.y = Math.random() * Math.PI * 4;
+
+      cube.rotation.z = Math.random() * Math.PI;
+      cube.rotation.x = Math.random() * (Math.PI / 6);
+      cube.updateMatrix();
+      this.scene.add(cube);
+
     }
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
       alpha: true,    // transparent background
       antialias: true // smooth edges
     });
-    this.renderer.setSize(window.innerWidth,window.innerHeight);
-    this.renderer.setPixelRatio( window.devicePixelRatio );
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
 
     // document.getElementById('engine-wrapper').appendChild( this.renderer.domElement );
     // document.addEventListener( 'mousemove', this.onDocumentMouseMove, false );
@@ -133,29 +134,29 @@ export class EngineService {
     requestAnimationFrame(() => {
       this.render();
     });
-    this.camera.position.y += ( - this.mouseY + 200 - this.camera.position.y ) * .05;
-    this.camera.lookAt( this.scene.position );
-    this.renderer.render( this.scene, this.camera );
-    var time = Date.now() *0.00002;
-				for ( var i = 0; i < this.scene.children.length; i ++ ) {
-					var object = this.scene.children[ i ];
-					
-						object.rotation.y = time * ( i < 4 ? ( i + 1 ) : - ( i + 1 ) );
-						if ( i < 5 ) {
-							var scale = object.userData.originalScale * ( i / 5 + 1 ) * ( 1 + 0.5 * Math.sin(7* time ) );
-							object.scale.x = object.scale.y = object.scale.z ;
-						}
-					
+    this.camera.position.y += (- this.mouseY + 200 - this.camera.position.y) * .05;
+    this.camera.lookAt(this.scene.position);
+    this.renderer.render(this.scene, this.camera);
+    var time = Date.now() * 0.00002;
+    for (var i = 0; i < this.scene.children.length; i++) {
+      var object = this.scene.children[i];
 
-    // this.renderer.render(this.scene, this.camera);
+      object.rotation.y = time * (i < 4 ? (i + 1) : - (i + 1));
+      if (i < 5) {
+        var scale = object.userData.originalScale * (i / 5 + 1) * (1 + 0.5 * Math.sin(7 * time));
+        object.scale.x = object.scale.y = object.scale.z;
+      }
+
+
+      // this.renderer.render(this.scene, this.camera);
+    }
   }
-}
 
   resize() {
     this.windowHalfX = window.innerWidth / 2;
     this.windowHalfY = window.innerHeight / 2;
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 }
