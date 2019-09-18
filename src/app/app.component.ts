fb80bd9795479317476d5cea548c7d59b457c457
Shujathlive <shujathlive@gmail.com>
2019-09-19 00:51:52 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component,OnInit,HostListener } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import * as luxy from 'luxy.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'fxmulti';
 
 
-  ngOnInit(){
-    luxy.init();
+  ngOnInit() {
+    var orientation = window.screen.orientation;
+    if (orientation.type === "landscape-primary") {
+      luxy.init();
+    }
 
   }
 }
