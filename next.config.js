f224833dc65ca03c11f2bea29acc6181c7de4e89
shujathkhan <shujathlive@gmail.com>
2021-05-03 13:21:22 +0530

    remove statically

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
 
 module.exports = (phase, { defaultConfig }) => {
+  
   return {
     ...defaultConfig,
-    assetPrefix: "https://cdn.statically.io/gh/shujathkhan/shujathkhan.github.io/gh-pages/",
-    trailingSlash: true
+    exportPathMap: () => ({
+      '/': { page: '/' },
+    }),
+    assetPrefix: "/shujathkhan.github.io",
   };
 };
 
